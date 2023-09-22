d5cda0a6282537894d71f9d5e7c4f01a57d8938a
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-09-22 10:01:04 +0530

    commented onclick functions in sidebar

--- components/admin/Sidebar.jsx
+++ components/admin/Sidebar.jsx
@@ -32,7 +32,9 @@ const Sidebar = () => {
   },[])
     return (
       <>
-        <div onClick={() => setShow(show => !show)} className="sm:hidden px-5 py-5 absolute"><MenuIcon className='text-3xl' /></div>
+        <div
+            // onClick={() => setShow(show => !show)}
+             className="sm:hidden px-5 py-5 absolute"><MenuIcon className='text-3xl' /></div>
         {show && <Card className="absolute z-2 md:relative md:z-0 bg-black !rounded-[0px] col-span-1 h-[100%] w-full  p-4 shadow-xl shadow-blue-gray-900/5">
           <div className="mb-2 p-4">
           <Link href="/" className='bg-blue-800 mb-10 hover:bg-blue-500 text-white px-3 py-2 rounded-[5px] text-center'>
@@ -47,17 +49,23 @@ const Sidebar = () => {
           </div>
           <List>
             <ListItem className="text-white text-xl my-5 hover:bg-slate-500 px-3 py-3">
-              <Link onClick={() => setShow(show => !show)} href="/admin/add-task">
+              <Link
+                  // onClick={() => setShow(show => !show)}
+                  href="/admin/add-task">
                   Add task
               </Link>
             </ListItem>
             <ListItem className="text-white text-xl my-5 hover:bg-slate-500 px-3 py-3">
-              <Link onClick={() => setShow(show => !show)} href="/admin/add-admin">
+              <Link
+                  // onClick={() => setShow(show => !show)}
+                  href="/admin/add-admin">
                 Add new admin
               </Link>
             </ListItem>
             <ListItem className="text-white text-xl my-5 hover:bg-slate-500 px-3 py-3">
-              <Link onClick={() => setShow(show => !show)} href="/admin/assign-scores">
+              <Link
+                  // onClick={() => setShow(show => !show)}
+                  href="/admin/assign-scores">
                 Assign scores
               </Link>
             </ListItem>
--- components/cap/Sidebar.jsx
+++ components/cap/Sidebar.jsx
@@ -32,7 +32,9 @@ const Sidebar = () => {
   },[])
     return (
       <>
-        <div onClick={() => setShow(show => !show)} className="sm:hidden px-5 py-5 absolute"><MenuIcon className='text-3xl' /></div>
+        <div
+            // onClick={() => setShow(show => !show)}
+             className="sm:hidden px-5 py-5 absolute"><MenuIcon className='text-3xl' /></div>
         {show && <Card className="absolute z-2 md:relative md:z-0 bg-black !rounded-[0px] col-span-1 h-screen w-full  p-4 shadow-xl shadow-blue-gray-900/5">
           <div className="mb-2 p-4">
             <Link href="/" className='mb-10 bg-blue-800 hover:bg-blue-500 text-white px-3 py-2 rounded-[5px] text-center'>
@@ -47,12 +49,16 @@ const Sidebar = () => {
           </div>
           <List>
             <ListItem className="text-white text-xl my-5 hover:bg-slate-500 px-3 py-3">
-              <Link onClick={() => setShow(show => !show)} href="/cap/view-tasks">
+              <Link
+                  // onClick={() => setShow(show => !show)}
+                    href="/cap/view-tasks">
                   View tasks
               </Link>
             </ListItem>
             <ListItem className="text-white text-xl my-5 hover:bg-slate-500 px-3 py-3">
-              <Link onClick={() => setShow(show => !show)} href="/cap/view-tasks">
+              <Link
+                  // onClick={() => setShow(show => !show)}
+                  href="/cap/view-tasks">
                   Help
               </Link>
             </ListItem>
