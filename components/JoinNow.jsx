b9340ebb2691299ef645711a7f7cc4404261ced2
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-09-25 01:52:01 +0530

    fuck offffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff

--- components/JoinNow.jsx
+++ components/JoinNow.jsx
@@ -3,10 +3,16 @@ import {useRouter} from "next/navigation";
 
 const JoinNow = () => {
     const router = useRouter()
+    const [isMobile, setIsMobile] = React.useState(false)
+    React.useEffect(() => {
+        if (window.innerWidth < 640) {
+            setIsMobile(true)
+        }
+    }, [])
     return (
         <Fragment>
-            <div className="flex items-center justify-center my-6 w-full">
-                <div className="flex bg-pink-700 p-10 w-[70%] gap-3 items-center justify-between    ">
+            <div className={`flex items-center justify-center my-6 w-full`}>
+                <div className={`flex ${isMobile ? 'flex-col' : 'flex-row'}  bg-pink-700 p-10 w-[70%] gap-3 items-center justify-between `}>
                     <div className="flex flex-col gap-2">
                         <h1 className="text-white font-bold text-3xl">Ready to win Exciting Prizes?</h1>
                         <p className="text-white">Become a part of India’s largest space conference</p>
