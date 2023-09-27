3e233c8bd86053f6e1ba2e88d64b4ce0a17e44b3
AB7zz <48093418+AB7zz@users.noreply.github.com>
2023-09-28 01:22:34 +0530

    removed pre loader

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     const {data: session} = useSession()
 
     const [loaderText, setLoaderText] = React.useState("スペースアップキューサット")
-    const [isLoading, setIsLoading] = React.useState(true)
+    const [isLoading, setIsLoading] = React.useState(false)
 
 
     useEffect(() => {
