277803281565dff0600dbf821807521bdef8b19d
AB7zz <abhinavcv007@gmail.com>
2024-10-17 09:53:42 +0530

    10%

--- app/api/users/referrals/route.js
+++ app/api/users/referrals/route.js
@@ -47,7 +47,7 @@ export async function POST(request) {
 
         if (isValid) {
             return NextResponse.json(
-                { isValid: true, message: "Valid referral code! ₹50 discount applied." },
+                { isValid: true, message: "Valid referral code! 10% discount applied." },
                 { status: 200, headers: corsHeaders }
             );
         } else {
